30f15bda9c6da3f72bbbda67d397e6cf5806d61b
Alex <alexlpaz@gmail.com>
2025-07-22 16:55:40 -0300

    Update app.py

--- meu_sorteio_times/app.py
+++ meu_sorteio_times/app.py
@@ -7,25 +7,26 @@ import pandas as pd
 from meu_sorteio_times.core import formar_times
 
 # -------------- Estilo do App --------------
-st.set_page_config(page_title="Sorteio de Times", page_icon="⚽", layout="centered")
-st.markdown("<h1 style='text-align: center;'>⚽ Sorteio de Times</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; font-size:18px;'>Distribua jogadores de forma equilibrada com goleiros fixos.</p>", unsafe_allow_html=True)
+st.set_page_config(page_title="Sorteio de Times - Pelada Monstra", page_icon="⚽", layout="centered")
+st.markdown("<h1 style='text-align: center;'>⚽ Sorteio de Times - Pelada Monstra</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-size:18px;'>Distribuição de jogadores de forma equilibrada com goleiros fixos.</p>", unsafe_allow_html=True)
 st.markdown("---")
 
 # ----------- Entrada de Dados -------------
 with st.expander("📋 Instruções", expanded=True):
     st.markdown("""
-    - Digite um jogador por linha no formato: `Nome,Ranking`.
-    - Acrescente `_Gol` ao nome dos goleiros.  
+    - Digite um jogador por linha no formato: 'Nome,Estrelas'.
+    - Acrescente '_Gol' ao nome dos goleiros.  
     - Exemplo:
-      ```
-      João_Gol,90
-      Maria,85
-      Pedro,80
-      ```
+      '''
+      João_Gol,3
+      José,5
+      Pedro,3
+      Andre,4
+      '''
     """)
 
-texto = st.text_area("Jogadores (nome,ranking):", height=250)
+texto = st.text_area("Jogadores (Nome,Estrelas):", height=250)
 
 # ----------- Processamento -----------------
 jogadores = []
@@ -38,8 +39,8 @@ for linha in texto.strip().split("\n"):
             rank = 0
         jogadores.append((nome.strip(), rank))
 
-if len(jogadores) < 6:
-    st.warning("Digite pelo menos 6 jogadores para formar times.")
+if len(jogadores) < 12:
+    st.warning("Digite pelo menos 12 jogadores para formar times.")
 else:
     if st.button("🎲 Sortear Times", use_container_width=True):
         times = formar_times(jogadores, num_por_time=6)
@@ -54,7 +55,7 @@ else:
                         nome = f"🧤 **{nome}** (Goleiro)"
                         rank_display = "⭐"
                     else:
-                        rank_display = f"`{rank}`"
+                        rank_display = "⭐" * rank
                     data.append({"Jogador": nome, "Ranking": rank_display})
                 df = pd.DataFrame(data)
                 st.dataframe(df, use_container_width=True, hide_index=True)
